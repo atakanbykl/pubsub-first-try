dd59b11a4b61b1cd1c36b62eba5095390684e5e2
Atakan Bıyıklı <atakanbiyikli@gmail.com>
2025-05-31 15:59:19 +0300

    json

--- src/main/java/Publisher.java
+++ src/main/java/Publisher.java
@@ -1,5 +1,6 @@
 import io.nats.client.Connection;
 import io.nats.client.Nats;
+import java.nio.charset.StandardCharsets;
 
 public class Publisher {
     public static void main(String[] args) throws Exception {
@@ -7,7 +8,11 @@ public class Publisher {
         Connection nc = Nats.connect("nats://localhost:4222");
 
         // "updates" kanalına mesaj gönder
-        nc.publish("updates", "naber".getBytes());
+        String jsonMessage = "{ \"id\": \"robot1\", \"battery\": 87, \"mode\": \"idle\" }";
+
+
+        // nc.publish("updates", "naber".getBytes());
+        nc.publish("updates", jsonMessage.getBytes(StandardCharsets.UTF_8));
 
         // Bağlantıyı kapat
         nc.close();
